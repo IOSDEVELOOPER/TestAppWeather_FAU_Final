6f732756616212e55c79517383b843dd2f175e99
IOSDEVOPS2201 <IosDeveloper273@yandex.ru>
2023-04-25 15:33:53 +0300

    Initial Commit

---
+++ TestAppWeather_FAU/TestAppWeather_FAUApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppWeather_FAUApp.swift
+//  TestAppWeather_FAU
+//
+//  Created by Андрей Федоров on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppWeather_FAUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
